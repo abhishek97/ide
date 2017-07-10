d6ee6f73348789b2c1b9b13feb8e60ed94b50617
abhishek97 <zapper.awake@gmail.com>
2017-07-10 22:13:27 +0530

    seperate script for deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+npm run build
+cd dist/
+git init
+git config user.name "Travis CI"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
