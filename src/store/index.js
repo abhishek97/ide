2082c20e5a06117ce5c23e8439f0163a4e1b5599
Travis CI <travis-botNoreply@gmail.com>
2017-07-11 00:06:34 +0530

    jsWorker path for production

--- src/store/index.js
+++ src/store/index.js
@@ -82,9 +82,11 @@ export default new Vuex.Store({
   },
   actions: {
     runJs (context, {code, input}) {
-        let jsWorker = new Worker('../../static/jsWorker.js')
-        if ( jsWorker.onmessage === null )
-            jsWorker = new Worker('../../ide/static/jsWorker.js') // hack to work with gh-pages:
+        let jsWorker = null
+        if ( process.env.NODE_ENV === 'production' )
+            jsWorker = new Worker('../../ide/static/jsWorker.js')
+        else
+            jsWorker = new Worker('../../static/jsWorker.js')
         input = JSON.stringify(input)
         jsWorker.postMessage({code, input})
         jsWorker.onmessage = function (e) {
